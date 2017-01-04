8c0cc3b8738b091c0c2e7d9f5f4121178753ea9e
Andre Raiça Silva <andre.silva@fcamara.com.br>
2017-01-04 16:03:25 -0200

    inclusão de POST e configurações

--- controllers/testeController.js
+++ controllers/testeController.js
@@ -3,4 +3,11 @@ module.exports = function(app){
     console.log("get req.");
     res.send('Get Sucesso');
   });
+
+  app.post('/teste', function(req, res){
+
+    var obj = req.body;
+    console.log("post: " + obj);
+    res.send('Post Sucesso');
+  });
 }
