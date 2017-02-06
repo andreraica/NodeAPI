aac6d751e3e809aeb9459a5325d928644a2ac9fa
Andre Raiça Silva <andre.silva@fcamara.com.br>
2017-02-06 10:57:49 -0200

    uso de http no azure

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var app = require('./config/custom-express')();
+//var app = require('./config/custom-express')();
 
-app.listen(3000, function(){
-  console.log('Servidor rodando na porta 3000.');
-});
+//app.listen(3000, function(){
+//  console.log('Servidor rodando na porta 3000.');
+//});
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+
+}).listen(process.env.PORT || 8080);
