2f10bdca4441702895c4085c1a8748fc60c6ea49
Andre Raiça Silva <andre.silva@fcamara.com.br>
2017-02-06 11:47:04 -0200

    variavel de ambiente para porta no server

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 //Local
-//var app = require('./config/custom-express')();
-//app.listen(3000, function(){
-//  console.log('Servidor rodando na porta 3000.');
-//});
-
-
-
-var app = require('express')();
-var server = require('http').createServer(app);
-
-server.listen(process.env.PORT || 8080, function(){
-  console.log('Servidor rodando na porta 8080.');
+var app = require('./config/custom-express')();
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Servidor rodando na porta. server<<' + process.env.PORT + '>> ou local<<3000>>');
 });
+
+
+
+//var app = require('express')();
+//var server = require('http').createServer(app);
+//server.listen(process.env.PORT || 8080, function(){
+//  console.log('Servidor rodando na porta 8080.');
+//});
 
 
 
